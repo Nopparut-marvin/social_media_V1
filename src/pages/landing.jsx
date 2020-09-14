1ff67afb5242cfc888a4f395ea9aef76252e984c
Nopparut-marvin <nopparut.bit@hotmail.com>
2020-09-14 23:30:14 +0700

    check when it bottom

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import Post from "../components/post";
 
-export default function landing() {
-  return (
+export default function Landing() {
+    const [lists, setLists] = useState(Array.from(Array(10).keys()))
+    function handleScroll() {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            console.log("fetch");
+        }
+      }
+    useEffect(() => {
+        window.addEventListener('scroll',handleScroll);
+        return () => {
+            window.removeEventListener('scroll',handleScroll);
+        }
+    }, [])
+return (
     <div>
-     <div className="container bg-gray-300 mx-auto">container</div>
+      <div className="w-full bg-gray-300">
+        <div className=" items-center container flex flex-col bg-gray-400 w-full mx-auto text-white font-bold text-2xl p-5">
+         <div className="mb-5">All Post</div>
+          {lists.map((listx,index) => <Post key={index} text={listx}></Post>)}
+        </div>
+      </div>
     </div>
   );
 }
